261d2e469b6859c047c1819d0d610946292f86bc
ayogatot <azerino25@gmail.com>
2019-08-06 13:05:12 +0700

    add update and delete http verb

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -18,6 +18,26 @@ exports.getAllBooks = async (req, res) => {
   }
 };
 
+exports.getBookById = (req, res) => {
+  try {
+    const bookId = books.books.filter(
+      item => item.id === Number(req.params.id)
+    );
+    console.log(bookId);
+    res.status(200).json({
+      error: false,
+      errorMessage: null,
+      data: bookId
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: true,
+      errorMessage: error,
+      data: []
+    });
+  }
+};
+
 exports.addBooks = async (req, res) => {
   try {
     const title = req.body.title;
@@ -39,7 +59,72 @@ exports.addBooks = async (req, res) => {
       data: data
     });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({
+      error: true,
+      errorMessage: error,
+      data: []
+    });
+  }
+};
+
+exports.updateBookById = (req, res) => {
+  try {
+    const data = {
+      title: req.body.title,
+      author: req.body.author
+    };
+    const updateBook = books.books.filter(item => {
+      if (item.id === Number(req.params.id)) {
+        return Object.assign(item, data);
+      } else {
+        return item;
+      }
+    });
+    res.status(200).json({
+      error: false,
+      errorMessage: null,
+      data: updateBook
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: true,
+      errorMessage: error,
+      data: []
+    });
+  }
+};
+
+exports.deleteAllBooks = (req, res) => {
+  try {
+    const deleteBooks = books.books.slice(0, books.books.length);
+    console.log(deleteBooks);
+
+    res.status(200).json({
+      error: false,
+      errorMessage: null,
+      data: deleteBooks
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: true,
+      errorMessage: error,
+      data: []
+    });
+  }
+};
+
+exports.deleteBookId = (req, res) => {
+  try {
+    const deleteBookId = books.books.filter(
+      item => item.id !== Number(req.params.id)
+    );
+    books.books = deleteBookId;
+    res.status(200).json({
+      error: false,
+      errorMessage: null,
+      data: deleteBookId
+    });
+  } catch (error) {
     res.status(500).json({
       error: true,
       errorMessage: error,
--- routes/books.js
+++ routes/books.js
@@ -5,6 +5,12 @@ const booksController = require("../controllers/booksController");
 
 Router.route("/")
   .get(booksController.getAllBooks)
-  .post(booksController.addBooks);
+  .post(booksController.addBooks)
+  .delete(booksController.deleteAllBooks);
+
+Router.route("/:id")
+  .get(booksController.getBookById)
+  .put(booksController.updateBookById)
+  .delete(booksController.deleteBookId);
 
 module.exports = Router;
